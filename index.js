c6df7359807190e2b9e5f9c75a6b538a7788ebb0
gitYoungren <youngrengitonga@gmail.com>
2022-11-25 15:22:26 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+//write your code here
+let num1 = 31
+let num2 = 2
+ const multiply = num1 * num2
+ console.log(multiply)
 
-// Write your code here
+const random =Math.floor(Math.random() * 10) + 1;
+console.log(random)
+
+let num3 = 24
+let num4 = 6
+ const mod = num3 / num4
+ console.log(mod)
+
+ const max = 20
+console.log(max)
